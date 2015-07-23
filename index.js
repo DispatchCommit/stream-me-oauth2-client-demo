28bc8e73cb8274466ab6107f5ad5bf33ea3805dc
Cameron Webley <cameron@vubeology.com>
2015-07-23 13:49:29 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,212 @@
+var express = require('express'),
+    expressSession = require('express-session'),
+    passport = require('passport'),
+    request = require('request'),
+    StreamMeStrategy = require('passport-streamme-oauth2'),
+    Users = require('./lib/users');
+
+/* *
+ * Express Setup
+ * * *
+ *
+ * - tells express where the view templates are located
+ * - view engine setup
+ */
+var app = express();
+
+app.set('views', 'views');
+app.set('view engine', 'ejs');
+
+/* *
+ * Express-Session Setup
+ * * *
+ *
+ * After user login, this stores user credentials in a coookie preventing unnecessary data transmission on subsequent requests
+ * This must be done before app.use(passport.initialize()) AND app.use(passport.session())
+ */
+app.use(expressSession({ secret: 'keyboard cat', cookie: { maxAge: 60000 }, resave: false, saveUninitialized: false}));
+
+/* *
+ * Passport Setup
+ * * *
+ *
+ * StreamMeStrategy: handles the StreamMe user's login, OAuth2 client scopes authorization, authorization_code retreival, and the token exchange
+ *  - implemented when passport.authenticate() is called with 'streamme' provided as the strategy.
+ *  - requires an ID, secret, and callbackURL of a StreamMe Oauth2 client (created at https://developers.stream.me/oauth)
+ *
+ * serializeUser: gives passport a uniquely identifying piece of a user object to store in the session
+ *   - must be provided in order for passport to support login sessions
+ *
+ * deserializeUser: tells passport how to retrieve further information about a user that is not stored in the session
+ *  - must be provided in order for passport to support login sessions
+ *  - typically involves a database lookup
+ *  - in this simplified case, passport uses the userId stored in the session to fetch a user object 
+ *     (contiaining a StreamMe authorization token and refresh token) from a crude in-memory user-store
+ * 
+ * passport.initialize() must be called before passport.session()
+ */
+ passport.use('streamme', 
+     new StreamMeStrategy({
+         clientID: 'a0b9555a-7878-47b2-b989-086cf034c430',
+         clientSecret: 'c2a0ad5eaf3ea0af8188916020e01a3abe8285b5',
+         callbackURL: '/users/redirect',
+     },
+     function(accessToken, refreshToken, profile, done) {
+        var userObj = Users.save(accessToken,refreshToken,profile)
+        if(!userObj) {
+            return done(new Error('failed-to-save-user'));
+        }
+        return done(null, userObj);
+     }
+ ));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    return done(null, Users.get(id));
+});
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+/* *
+ * Middleware
+ * * */
+var requireLogin = function(req,res,next){
+    if(!req.user){
+        return res.redirect('/');
+    }
+    next();
+};
+
+/* *
+ * Route Controllers
+ * * *
+ *
+ * - Simple GET requests directed towards an authenticated StreamMe resource
+ *
+ * getFeed: retrieve a  user's message feed
+ * getEmoticons: retreive the list of user's custom emoticons. may be empty if the user has not created custom emoticons at https://www.stream.me/profile#emoticons
+ */
+var getFeed = function(req,res){
+    request({
+        url: 'https://www.stream.me/api-message/v1/users/' + req.user.slug + '/feed',
+        json: true,
+        headers: {
+            'Authorization': 'Bearer ' + req.user.at
+        }
+    },function(err,response,body){
+        if(err){
+            return res.status(500).send(err.message);
+        }
+        if(!response || response.statusCode !== 200){
+            return res.status(response && response.statusCode || 400).send('something-went-wrong');
+        }
+        res.render('user-data', {
+            username: req.user.username,
+            routename: 'feed',
+            data: JSON.stringify(body,null,4)
+        });
+    });
+};
+var getEmoticons = function(req,res){
+    request({
+        url: 'https://www.stream.me/api-emoticon/v1/' + req.user.slug + '/manage',
+        json: true,
+        headers: {
+            'Authorization': 'Bearer ' + req.user.at,
+        }
+    },function(err,response,body){
+        if(err){
+            return res.status(500).send(err.message);
+        }
+        if(!response || response.statusCode !== 200){
+            return res.status(response && response.statusCode || 400).send('something-went-wrong');
+        }
+        res.render('user-data', {
+            username: req.user.username,
+            routename: 'emoticons',
+            data: JSON.stringify(body,null,4)
+        });
+    });
+};
+
+/* *
+ * Express server and routing
+ * * */
+ app.listen(3000, function() {
+   console.log('Server listening on port 3000');
+ });
+
+/* *
+ * GET home page (no authentication)
+ * * */
+app.get('/', function(req, res) {
+  res.render('index', { title: 'OAuth2 Demo Client', username: req.user && req.user.username });
+});
+
+/* *
+ * Login
+ * * *
+ *
+ *  - directs the user to login at stream.me
+ *  - allows the user to accept permissions (scopes) requested by the OAuth2 client
+ *  - retrieves the user's authorization_code as specified in the OAuth2 spec (https://tools.ietf.org/html/rfc6749)
+ *  - exchanges the authorization_code for a StreamMe token
+ *  - redirects the user to the OAuth2 redirectURL given to the app
+ */
+app.get('/login', passport.authenticate('streamme', {}));
+
+/* *
+ * Logout
+ * * *
+ *
+ *  - Removes the user from the UsersStore and redirects the user home
+ *  - Calls logout(), a method that passport exposes which removes req.user and claers the session
+ */
+app.get('/logout', 
+    requireLogin,
+    function(req,res){
+        Users.delete(req.user.id);
+        req.logout();
+        res.redirect('/');
+    }
+)
+
+/* *
+ * Redirect
+ * * *
+ *
+ *  - this is the redirectURL specified in the StreamMe OAuth2 client at https://developers.stream.me/oauth
+ *  - in this example it just redirects the user home on authentication success or failure
+ */
+app.get('/users/redirect',
+    passport.authenticate('streamme', {
+    successRedirect: '/',
+    failureRedirect: '/'
+}));
+
+/* *
+ * Feed
+ * * *
+ *
+ *  - returns a user's message feed
+ *  - this route is authenticated and requires a valid access token
+ */
+app.get('/feed',
+    requireLogin,
+    getFeed
+);
+
+/* *
+ * Emoticons
+ * * *
+ *
+ *  - returns a user's custom emoticons (user and channel).
+ *  - this route is authenticated and requires a valid access token from an OAuth2 client app with the 'emoticon' scope
+ */
+app.get('/emoticons',
+    requireLogin,
+    getEmoticons
+);
---
+++ readme.md
@@ -0,0 +1,5 @@
+# <img src="https://static1.stream.me/web/active/images/robot-avatar.png" width=30px; style="display= inline-block" /> StreamMe Oauth2 Demo Client
+
+## Alpha
+
+Currently oauth clients are in alpha.  Please contact support@stream.me staff for an oauth client
